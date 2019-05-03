f873e6184121d71eee64957f34b1b634578d8fa3
blahkheart <dannithomx@gmail.com>
2019-05-03 17:04:09 +0100

    added transactions page

--- resources/views/includes/balance.blade.php
+++ resources/views/includes/balance.blade.php
@@ -1,5 +1,10 @@
-<div class="balance">
-    <p class="balance__text">
-        <a href="#" class="balance__link">0.00NGN</a>
-    </p>
+<div class="balance dropdown">
+    <a href="#" class="balance__text dropdown-toggle" role="button" id="dropdownMenu1" 
+    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        0.00NGN 
+    </a>
+    <div  class="balance__dropdown dropdown-menu dropdown-menu-right" aria-labelledby="DropdownMenu1">
+        <a href="#" class="dropdown-item balance__dropdown--link">Withdraw</a>
+        <a href="#" class="dropdown-item balance__dropdown--link">Deposit</a>
+    </div>
 </div>
--- resources/views/includes/farmsCard.blade.php
+++ resources/views/includes/farmsCard.blade.php
@@ -1,6 +1,6 @@
 <div class="content-wrap card-box">
     <div class="farms__display card">
-        <a href="#" class="card__info"> <img src="img/Chick-2.jpg" alt="hen image" class="farms__image card-img"></a>
+        <a href="#" class="card__info"> <img src="{{asset('img/Chick-2.jpg')}}" alt="hen image" class="farms__image card-img"></a>
         <h3 class="heading__tertiary card__heading text-center">19<sup>th</sup>  sponsoring window </h3>
         <div class="farms__text ">
             <p>Farm cycle: 7th April to 24th April</p> 
