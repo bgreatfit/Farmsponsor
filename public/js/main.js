c35a897c678d144e273bc71c6c75fbdba7f16e5d
blahkheart <dannithomx@gmail.com>
2019-05-10 15:43:31 +0100

    vestbank deposit view added

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,13 @@
+var capital = document.getElementById('vestbank-withdraw__capital');
+var interest = document.getElementById('vestbank-withdraw__interest');
+var all = document.getElementById('vestbank-withdraw__all');
 var other = document.getElementById('vestbank-withdraw__other');
+
 var amount = document.querySelector('.vestbank-withdraw__amount--label');
 
 amount.style.display = 'none';
+
+var choice = " ";
 
 function showAmount() {
     if(!other.checked) {
@@ -12,8 +18,26 @@ function showAmount() {
         amount.style.display = "block";
 
     }
+
+    // switch (choice){
+    //     case "capital":
+    //         capital.checked;
+    //         amount.style.display = "none";
+    //     break;
+    //     case "interest":
+    //         interest.checked;
+    //         amount.style.display = "none";
+    //     break;
+    //     case "all":
+    //         all.checked;
+    //         amount.style.display = "none";
+    //     break;
+    //     case "other":
+    //         other.checked;
+    //         amount.style.display = "block";
+    //     break;
+    // }
     
 }
-// showAmount();
 
 other.addEventListener('click', showAmount);
