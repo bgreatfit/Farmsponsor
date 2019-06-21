15c161b1370dcdf32035cba9a6149c61cac61d21
drumzminister <ishukpong418@gmail.com>
2019-06-21 10:04:30 +0100

    dashboard link on homepage

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -4,15 +4,15 @@
                 <a class="navbar-brand navi-bar__brand" href="{{ url('/') }}">
                     <img src="{{asset('img/farmsponsor-logo.svg')}}" alt="Farmsponsor logo" class="navi-bar__picture">
                 </a>
-        
+
                 <button class="navbar-toggler navigation__toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon navigation__toggler--icon"></span>
                 </button>
-        
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navigation-menu">
                         <!-- Authentication Links -->
-        
+
                             <li class="nav-item navigation__item">
                                 <a class="nav-link navigation__item--link" href="{{route('about')}}">About us</a>
                             </li>
@@ -26,7 +26,7 @@
                                 <a id="navbarDropdown" class="nav-link navigation__item--link dropdown-toggle"
                                 href="#" role="button" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false" v-pre>Get help</a>
-        
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item dropdown-item__link" href="{{ route('faq') }}">FAQs</a>
                                     <a class="dropdown-item dropdown-item__link" href="#">Help Center</a>
@@ -35,15 +35,28 @@
                             <li class="nav-item navigation__item">
                                 <a class="nav-link navigation__item--link" href="https://farmsponsor.com.ng/latest-updates" target="_blank">Blog</a>
                             </li>
+                            @auth
                             <li class="nav-item navigation__item">
-                                <a class="nav-link navigation__item--link" href="{{route('dashboard')}}" target="_blank">Dashboard</a>
+                                <a class="nav-link navigation__item--link" href="{{route('dashboard')}}">Dashboard</a>
                             </li>
+
+                            <li class="nav-item navigation__item">
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link navigation__item--link nav-link-btn">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                            @else
                             <li class="nav-item navigation__item">
                                 <a a class="nav-link navigation__item--link nav-link-signUp" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
                             </li>
                             <li class="nav-item navigation__item">
                                 <a a class="nav-link navigation__item--link nav-link-btn" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
+                            @endauth
                     </ul>
                 </div>
             </div>
