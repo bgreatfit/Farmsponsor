69c6bbecbf2e35d21bb8ed03992f37acdd1e4139
Drumzminister <ishukpong418@gmail.com>
2019-06-30 15:37:04 +0100

    final

--- app/Console/Commands/vestbanking.php
+++ app/Console/Commands/vestbanking.php
@@ -41,7 +41,7 @@ class vestbanking extends Command
         $vestors = Vestbank::all();
 
        foreach($vestors as $vestor){
-           $rate = 0.0004464;
+           $rate = 0.0004164;
 
 
            $dailyInterest = $vestor->capital * $rate;
