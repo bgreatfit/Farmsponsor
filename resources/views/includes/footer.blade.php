305669007fb58b5f8efb87c45dc18e4129122052
blahkheart <dannithomx@gmail.com>
2019-07-04 16:52:19 +0100

    minor tweaks to footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -77,11 +77,7 @@
                                 <div class="footer__item"><a href="{{route('about')}}" class="footer__links">About</a></div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-12 col-md-12">
-                                <div class="footer__item"><a href="{{route('aboutVestbanking')}}" class="footer__links">Vestbanking</a></div>
-                            </div>
-                        </div>
+                       
                         <div class="row">
                             <div class="col-12 col-md-12">
                                 <div class="footer__item"><a href="#" target="_blank" class="footer__links">Blog</a></div>
@@ -115,6 +111,11 @@
                         <div class="row">
                             <div class="col-12 col-md-12">
                                 <div class="footer__item"><a href="{{route('vestbanking')}}" class="footer__links">Get involved</a></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-md-12">
+                                <div class="footer__item"><a href="{{route('aboutVestbanking')}}" class="footer__links">Vestbanking</a></div>
                             </div>
                         </div>
                     </div>
