cb23c3c4e862c523d8b7ebf83d0870828f0c6245
blahkheart <dannithomx@gmail.com>
2019-07-15 11:55:19 +0100

    deposit modal disabled to suspend payments

--- resources/views/email/vestbank-pay-receipt.blade.php
+++ resources/views/email/vestbank-pay-receipt.blade.php
@@ -361,7 +361,7 @@
                                                             </a>
                                                         </div>
                                                         <div class="introductionHeading">
-                                                            <h1 class="introductionHeadingMain">Sponsor’s Receipt</h1>
+                                                            <h1 class="introductionHeadingMain">Vestbanking Payment schedule</h1>
                                                         </div>
                                                     </td>
                                                 </tr>
