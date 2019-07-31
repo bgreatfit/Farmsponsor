3d5555bedd22efee3699a904e526390739b55730
Drumzminister <ishukpong418@gmail.com>
2019-07-31 13:52:22 +0100

    fixed users

--- resources/views/includes/usersSearch.blade.php
+++ resources/views/includes/usersSearch.blade.php
@@ -1,9 +1,9 @@
-<form action=" " method="GET" class="active-cyan-4 searchbar__form">
+<form action="{{route('users.searchname')}}" method="GET" class="active-cyan-4 searchbar__form">
     <div class="input-group mb-3">
-        <input type="text" class="form-control searchbar__input" placeholder="Enter name" name="value" value=" ">
+        <input type="text" class="form-control searchbar__input" placeholder="Enter name" name="value" value="{{$searchValue ?? ' '}}">
         <div class="input-group-append searchbar__button-box">
             <button class="btn btn-secondary searchbar__button" type="submit">Search</button>
-            <a class="btn btn-warning searchbar__button" href="#">Clear Search</a>
+            <a class="btn btn-warning searchbar__button" href="{{route('admin.users')}}">Clear Search</a>
         </div>
     </div>
 </form>
